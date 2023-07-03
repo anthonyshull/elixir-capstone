f2a5307c51c376dbe7ac4856536b9e490376034a
Anthony Shull <shull.anthony@gmail.com>
2023-07-03 09:48:26 -0500

    split out functions

--- lib/capstone/pipelines/airport.ex
+++ lib/capstone/pipelines/airport.ex
@@ -27,30 +27,34 @@ defmodule Capstone.Pipeline.Airport do
     Enum.map(message.data, fn airport ->
       channel = message.metadata.amqp_channel
       payload = airport |> Jason.encode!()
+      routing_key = if airport.grid_id == nil, do: "grid_pipeline", else: "weather_pipeline"
 
-      if airport.grid_id == nil do
-        AMQP.Basic.publish(channel, "", "grid_pipeline", payload)
-      else
-        AMQP.Basic.publish(channel, "", "weather_pipeline", payload)
-      end
+      AMQP.Basic.publish(channel, "", routing_key, payload)
     end)
 
     message
   end
 
   def prepare_messages(messages, _context) do
-    messages_by_city_state = Enum.group_by(messages, fn message ->
+    grouped_messages = group_messages(messages)
+    airports = Airport.in_cities_states(grouped_messages |> Map.keys())
+
+    put_airports_in_messages(grouped_messages, airports)
+  end
+
+  defp group_messages(messages) do
+    Enum.group_by(messages, fn message ->
       message.data |> Jason.decode!() |> Map.take(["city", "state"]) |> Map.values() |> List.to_tuple()
     end)
+  end
 
-    airports = Airport.in_cities_states(messages_by_city_state |> Map.keys())
-
-    Enum.map(messages_by_city_state, fn {city_state, messages} ->
+  defp put_airports_in_messages(grouped_messages, airports) do
+    Enum.map(grouped_messages, fn {city_state, messages} ->
       airports = Map.get(airports, city_state, [])
 
       [head | tail] = messages
 
-      Enum.map(tail, fn message -> Broadway.Message.put_data(message, inspect(city_state)) end)
+      Enum.map(tail, fn message -> Broadway.Message.put_data(message, []) end)
       |> List.insert_at(0, Broadway.Message.put_data(head, airports))
     end) |> List.flatten()
   end
